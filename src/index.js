2e3288634398653494994a5040efdf78a69b275b
aidan-gee <aidangeewd@gmail.com>
2016-09-21 20:22:19 +0100

    Reverted back before react app was rejects, installed a service worker loader instead.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import Checkout from './components/Checkout';
 import './index.css';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
+const register = require("serviceworker!./sw.js");
+register({ scope: '/' })
+  .then(() => console.log('It worked!'))
+  .catch(err => console.log('It didnt work!', err));
+
 
 ReactDOM.render(
   <Router history={browserHistory}>
